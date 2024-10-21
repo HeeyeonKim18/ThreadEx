1ef184cead0d6dd6a17163d7eca6afb767781ccd
HeeyeonKim18 <go1927hm@gmail.com>
2024-10-21 10:50:24 +0900

    threadsafe issues

---
+++ src/main/java/org/example/javaconcurrency/threadsafe/ResultErrorResoultion.java
@@ -0,0 +1,24 @@
+package org.example.javaconcurrency.threadsafe;
+
+public class ResultErrorResoultion {
+
+    static int count;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = ()->{
+            synchronized (ResultErrorResoultion.class) {
+                for(int i = 0; i < 10000; i++){
+                    count++;
+                }
+            }
+        };
+
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(count); // the result should be 20000
+    }
+}
